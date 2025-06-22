6fc248fdf5b7d7fc4ec2c662d223a92200301ee8
KeerthiKelam-minfy <keerthi.kelam@minfytech.com>
2025-06-23 00:56:01 +0530

    add open API spec

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import userRoute from "./routes/user.routes.js"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
+import swaggerJSDoc from "swagger-jsdoc"
+import swaggerUi from "swagger-ui-express"
 
 import cors from "cors";
 
@@ -13,6 +15,33 @@ app.use(cors()); // Allow cross-origin requests
 connectDB()
 
 app.use(express.json())
+
+
+// --- Swagger-JSDoc Configuration ---
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Simple Users API (Auto-generated)',
+      version: '1.0.0',
+      description: 'A basic API for managing users, documented with Swagger-JSDoc.',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  // Path to the API docs
+  apis: ['./routes/*.js'],
+};
+
+// Generate the OpenAPI specification
+const openapiSpecification = swaggerJSDoc(options);
+
+// --- Serve the Swagger UI ---
+// You can delete the yaml.load() part now
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 app.get('/', (req, res) => {
     res.send('Cheat Buster API is running!');
